fe608b2d574d3583900d6d44a636d174509a7d01
Neil Macneale <neil.macneale.v@gmail.com>
2020-09-20 11:52:52 -0700

    Can write new data to textures

--- texture.go
+++ texture.go
@@ -11,10 +11,42 @@ import (
 )
 
 type Texture struct {
+  dimension int
   id uint32
+  width, height, depth int
 }
 
-func NewTexure2DFromData(width, height int, data []uint8) *Texture {
+func NewTexture3DFromData(width, height, depth int, data []uint8) *Texture {
+  var id uint32
+  gl.GenTextures(1, &id)
+  gl.BindTexture(gl.TEXTURE_3D, id)
+  gl.TexParameteri(gl.TEXTURE_3D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+  gl.TexParameteri(gl.TEXTURE_3D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+  gl.TexParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+  gl.TexParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+  gl.TexImage3D(
+    gl.TEXTURE_3D,
+    0,
+    gl.RGBA,
+    int32(width),
+    int32(height),
+    int32(depth),
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    gl.Ptr(data),
+  )
+
+  t := Texture{}
+  t.id = id
+  t.width = width
+  t.height = height
+  t.depth = depth
+  t.dimension = 3
+  return &t
+}
+
+func NewTexture2DFromData(width, height int, data []uint8) *Texture {
   var id uint32
   gl.GenTextures(1, &id)
   gl.BindTexture(gl.TEXTURE_2D, id)
@@ -36,6 +68,9 @@ func NewTexure2DFromData(width, height int, data []uint8) *Texture {
 
   t := Texture{}
   t.id = id
+  t.width = width
+  t.height = height
+  t.dimension = 2
   return &t
 }
 
@@ -50,14 +85,52 @@ func NewTexture2DFromFile(path string) (*Texture, error) {
   }
 
   rgba := image.NewRGBA(img.Bounds())
-  if rgba.Stride != rgba.Rect.Size().X*4 {
+  if rgba.Stride != rgba.Rect.Size().X * 4 {
     return nil, fmt.Errorf("Unsupported stride")
   }
   draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
 
-  return NewTexure2DFromData(rgba.Rect.Size().X, rgba.Rect.Size().Y, rgba.Pix), nil
+  return NewTexture2DFromData(rgba.Rect.Size().X, rgba.Rect.Size().Y, rgba.Pix), nil
+}
+
+func (t *Texture) Data(data []uint8) {
+  t.Bind()
+  if t.dimension == 2 {
+    gl.TexImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      int32(t.width),
+      int32(t.height),
+      0,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      gl.Ptr(data),
+    )
+  } else if t.dimension == 3 {
+    gl.TexImage3D(
+      gl.TEXTURE_3D,
+      0,
+      gl.RGBA,
+      int32(t.width),
+      int32(t.height),
+      int32(t.depth),
+      0,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      gl.Ptr(data),
+    )
+  } else {
+    panic("Unsuported dimension! (should be 2 or 3 for 2d or 3d textures)")
+  }
 }
 
 func (t *Texture) Bind() {
-  gl.BindTexture(gl.TEXTURE_2D, t.id)
+  if t.dimension == 2 {
+    gl.BindTexture(gl.TEXTURE_2D, t.id)
+  } else if t.dimension == 3 {
+    gl.BindTexture(gl.TEXTURE_3D, t.id)
+  } else {
+    panic("Unsuported dimension! (should be 2 or 3 for 2d or 3d textures)")
+  }
 }
